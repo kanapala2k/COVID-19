c7365acfd4a762ef2685499c6a0d015f0751d9d5
hafizbhuyan <hafiz.bhuyan0910@gmail.com>
2025-08-14 12:48:35 -0400

    connecting to Elasticsearch

---
+++ api/elasticsearch_client.py
@@ -0,0 +1,13 @@
+from elasticsearch import Elasticsearch
+
+# Connect to Elasticsearch
+es = Elasticsearch(
+    "https://my-elasticsearch-project-dcdc60.es.us-east-1.aws.elastic.cloud:443",
+    api_key="WXY3dHBKZ0J0aFZlM1hLRWp2V206cmlmR1Q0ODlkOGNCU1BVaTBsUFpLdw=="
+)
+
+# Test connection
+if es.ping():
+    print("Connected to Elasticsearch!")
+else:
+    print("Connection failed.")
