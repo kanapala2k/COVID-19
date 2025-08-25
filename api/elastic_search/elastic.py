67e09ae168f1f71eeda446799cf50e4d0f36c02e
hafizbhuyan <hafiz.bhuyan0910@gmail.com>
2025-08-25 11:29:47 -0400

    the initial work for facet search and separating the searches into files

--- api/elastic_search/elastic.py
+++ api/elastic_search/elastic.py
@@ -5,29 +5,3 @@ es = Elasticsearch(
     "https://my-elasticsearch-project-dcdc60.es.us-east-1.aws.elastic.cloud:443",
     api_key="WXY3dHBKZ0J0aFZlM1hLRWp2V206cmlmR1Q0ODlkOGNCU1BVaTBsUFpLdw=="
 )
-
-# Test connection
-if es.ping():
-    print("Connected to Elasticsearch!")
-else:
-    print("Connection failed.")
-
-def multi_match_search(query: str):
-    retriever_object = {
-        "standard": {
-            "query": {
-                "multi_match": {
-                    "query": query,
-                    "fields": [
-                        "title",
-                        "abstract"
-                    ]
-                }
-            }
-        }
-    }
-    search_response = es.search(
-        index="covid_19",
-        retriever=retriever_object
-    )
-    return search_response["hits"]["hits"]
