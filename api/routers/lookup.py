c9158241f222659dc9526e4954eb627a0d1e5669
Victor Kingue <victor.kingue@navitastech.com>
2025-08-27 17:31:44 -0400

    Added a PUT method, New Field format

--- api/routers/lookup.py
+++ api/routers/lookup.py
@@ -2,6 +2,10 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/lookup/")
+@router.get("/lookup/{str}")
+async def lookup(query: str):
+    return {"looking up": query }
+
+@router.put("/lookup/{str}")
 async def lookup(query: str):
     return {"looking up": query }
--- api/routers/search.py
+++ api/routers/search.py
@@ -4,7 +4,13 @@ import elastic_search.elastic
 
 router = APIRouter()
 
-@router.get("/search/")
+@router.get("/search/{str}")
+async def lookup(query: str):
+    expanded_query = query_expansion.sentence_transformer_wrapper.expanded_query(query)
+    print("expanded_query", expanded_query)
+    return elastic_search.elastic.multiMatchSearch(query=expanded_query)
+
+@router.put("/search/{str}")
 async def lookup(query: str):
     expanded_query = query_expansion.sentence_transformer_wrapper.expanded_query(query)
     print("expanded_query", expanded_query)
